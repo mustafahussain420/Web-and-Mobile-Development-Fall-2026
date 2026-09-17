7f2d70d1ec7b32fef89278040fec5f097fea89ab
mustafahussain420 <mustafa500h@gmail.com>
2026-09-17 12:02:02 +0500

    Import car to App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import { Car } from './models/car'
 
 function App() {
   return (
